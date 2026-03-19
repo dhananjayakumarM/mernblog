413a21673b3c854f2dd8642c0389323cf123d052
dhananjayakumarM <dhananjayvarma123@gmail.com>
2026-03-19 09:33:19 +0530

    added pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-3xl font-bold text-gray-800">
-          Vite ⚡ + React ⚛️ + Tailwind CSS 🎨
-        </h1>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/sign-in" element={<Signin />}></Route>
+          <Route path="/sign-up" element={<Signup />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
